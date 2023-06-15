9b4df6c56979acc9f3dea572181a3aff13807ed4
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-15 16:52:47 -0700

    Print with the F

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include stdio.h
+
+/**
+ * Main: prints using printf
+ * Return: Returns 0
+ */
+int void(main)
+{
+	Printf ("with proper grammar, but the outcome is a piece of art,\n");
+	Return(0);
+}
