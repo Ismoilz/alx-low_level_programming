e263e37b941f604bf110f4820c723ca77df5a5b9
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-16 14:24:54 -0700

    program lists alphabet from a to z except the letter e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - Entry point of the program.
+ *
+ * Description: This program lists the alphabet from 'a' to 'z' except 'e'
+ *		using a while loop and putchar.
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char x;
+
+	x = 'a';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+		if (x == 'e')
+		{
+			continue;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
