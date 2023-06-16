32aeaed4149b1577e37af5769697e28f2d0c90e3
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-15 17:25:59 -0700

    Print with the F

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints exactly with proper grammar, but the outcome is a piece of art, followed by a new line
+ * main - prints exactly with proper grammar, 
+ * but the outcome is a piece of art, 
+ * followed by a new line
  * Return: returns 0
  */
 int main(void)
