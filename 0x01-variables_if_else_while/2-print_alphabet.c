789e56da17c8a628b00ec2d33a2ba0aaac86bc4d
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-16 14:01:18 -0700

    Lists lowercase alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,8 +2,8 @@
 /**
  * main - Entry point of the program.
  *
- * Description: This program lists the alphabet from 'a' to 'z' using a while loop and putchar.
- *
+ * Description: This program lists the alphabet from 'a' to 'z' 
+ * 		using a while loop and putchar.
  * Return: 0 (Success)
  */
 int main(void)
