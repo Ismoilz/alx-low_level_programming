5117c1307b36928387f577343831f9182d6efc4c
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-16 16:57:16 -0700

    lists combination of 3 digits

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -14,33 +14,33 @@ int main(void)
 
     while (x <= 7)
     {
-        y = x + 1;
+    y = x + 1;
 
-        while (y <= 8)
-        {
-            z = y + 1;
+    while (y <= 8)
+    {
+    z = y + 1;
 
-            while (z <= 9)
-            {
-                putchar(x + '0');
-                putchar(y + '0');
-                putchar(z + '0');
+    while (z <= 9)
+    {
+    putchar(x + '0');
+    putchar(y + '0');
+    putchar(z + '0');
 
-                if (!(x == 7 && y == 8 && z == 9))
-                {
-                    putchar(',');
-                    putchar(' ');
-                }
+    if (!(x == 7 && y == 8 && z == 9))
+    {
+    putchar(',');
+    putchar(' ');
+    }
 
-                z++;
-            }
+    z++;
+    }
 
-            y++;
-        }
+    y++;
+    }
 
-        x++;
+    x++;
     }
 
     putchar('\n');
-    return 0;
-}
+    return(0);
+    }
