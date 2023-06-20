a16f04753b045511fdeea2f344537fb9201048cc
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-20 16:12:02 -0700

    Checks for alphabetic value

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic value
+ * @c: character to verify
+ * Return: 1 if alphabetic 0 otherwise
+ */
+
+int _isalpha(char c)
+{
+	while (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		{
+		return (1);
+	}
+	return (0);
+}
