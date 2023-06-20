adb754ef49fa300d5cf844d43599e277dff8269d
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-20 14:03:00 -0700

    program to list alphabet lowercase

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Description: Prints lower case alphabet
+ * Return: 0 (success)
+ */
+
+void print_alphabet (void)
+{
+	char x;
+	x = 'a';
+	while(x < 'z'+1)
+	{
+		_putchar(x);
+		x++;
+	}
+	_putchar('\n');
+}
