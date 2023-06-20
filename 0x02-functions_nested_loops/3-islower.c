272cc577b60f74e79bedfc517cf4f603ffe87af0
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-20 16:01:54 -0700

    Checks for lowercase alphabet

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,13 @@
 
 /**
  * _islower - checks for lowercase
- * c: variable to check
+ * @c: variable to check
  * Return: 1 if lowercase 0 otherwise
  */
 
 int _islower(char c)
 {
-	while (c >= 'a'&& c <= 'z')
+	while (c >= 'a' && c <= 'z')
 	{
 		return (1);
 	}
