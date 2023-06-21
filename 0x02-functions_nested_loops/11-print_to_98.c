1e72841de488ae3da21d72d31dda56bc6f1c3a35
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-20 19:05:07 -0700

    counts to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+void count_to_98(int n)
+{
+	if (n > 98) {
+		printf("%d\n", 98);
+		return;
+	}
+
+	while (n <= 98) {
+		printf("%d", n);
+	if (n != 98) {
+		printf(", ");
+	}
+
+	n++;
+	}
+	printf("\n");
+}
