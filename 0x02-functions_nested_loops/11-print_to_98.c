2c902f9f5130eb038ce0886ad651cfd1cdaf858b
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-20 19:25:15 -0700

    counts to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -21,17 +21,19 @@ void print_to_98(int n)
 		printf("%d", n);
 	if (n != 98)
 		printf(", ");
-	
+
 
 	n++;
 	}
-	
-	} else {
-		while (n >= 98) {
+
+	}
+	else
+	{
+		while (n >= 98)
+		{
 			printf("%d", n);
-			if (n != 98) {
+			if (n != 98)
 				printf(", ");
-			}
 			n--;
 	}
 	}
