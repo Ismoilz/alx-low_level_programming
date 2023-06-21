630c7ede572a9cb8b7e93fe54598181dd410e158
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-21 14:37:23 -0700

    Adding 0-main

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-* main - tests function that prints if integer is positive or negative
+* main - entry point
+* positive_or_negative: tests function that prints if integer is positive or negative
 * Return: 0
 */
 
