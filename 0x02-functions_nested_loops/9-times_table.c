b2c060ae0f7d9519d89942b42f72010c2c144690
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-20 18:35:11 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,30 +8,22 @@ void times_table(void)
 {
 	int x, y, n;
 
-	for (x = 0; x < 10; x++)
-	{
-		for (y = 0; y < 10; y++)
-		{
-			n = x * y;
-			if (y == 0)
-			{
-				_putchar(n + '0');
-			}
-			if (n)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(n + '0');
-			}
-			else if (n >= 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar((n / 10) + '0');
-				_putchar((n % 10) + '0');
-			}
-		}
-		_putchar('\n');
-	}
+    for (x = 0; x < 10; x++) {
+        for (y = 0; y < 10; y++) {
+            n = x * y;
+            if (y == 0) {
+                _putchar(n + '0');
+            } else {
+                _putchar(',');
+                _putchar(' ');
+                if (n >= 10) {
+                    _putchar((n / 10) + '0');
+                } else {
+                    _putchar(' ');
+                }
+                _putchar((n % 10) + '0');
+            }
+        }
+        _putchar('\n');
+    }
 }
