db9a48d5a3b6eaa87e6f478ef4d453f57e66ba68
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-22 15:22:28 -0700

    prints from 0 to 9 without 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints from 0 to 9 without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9 && i != 2 && i != 4)
+	{
+		_putchar(i + 48);
+		i++;
+	}
+
+	_putchar('\n');
+}
