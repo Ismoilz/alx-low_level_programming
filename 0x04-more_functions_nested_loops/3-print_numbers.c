7cc476fc8be98368c60bbdd2948066ccd4863a89
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-22 14:48:42 -0700

    prints from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints from 0 to 9
+ * Return: 0 (success)
+ */
+
+void print_numbers(void);
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		_putchar('i');
+		i++;
+	}
+
+	_putchar('\n');
+
+	return (0);
+}
