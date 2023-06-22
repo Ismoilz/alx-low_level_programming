daa360dd7cd1a637af8018b7b53a7d91f309346b
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-22 14:09:20 -0700

    Checks if c is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * int _isupper - checks if letter is upper case or lower case
+ * Return: 1 if upper 0 if otherwise
+ */
+
+int _isupper(int c);
+{
+	if(c >= 'A' && c <= 'Z')
+	{
+	return (1);
+	}
+	else{
+	return (0);
+	}
+}	
