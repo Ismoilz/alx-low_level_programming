aa0826b8e4ef10251907e85043945def6f824450
Ismoilz <ismailelhoumam1@gmail.com>
2023-06-22 17:08:47 -0700

    prints line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *print_line - prints a line with as many _ as requested
+ *@n: line number
+ */
+void print_line(int n)
+{
+	int i;
+	
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+	{
+	_putchar('_');
+	}
+	}
+	_putchar('\n');
+}
