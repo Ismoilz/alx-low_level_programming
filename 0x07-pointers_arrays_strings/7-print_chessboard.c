d1a2e694d44ea377b0acec771ae80a6af51d5e58
Ismoilz <ismailelhoumam1@gmail.com>
2023-07-03 19:19:50 -0700

    function to print chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -5,12 +5,17 @@
  * @a: array to print
  */
 
-void print_chessboard(char (*a)[8]) {
-    int i, j;
-    for (i = 0; i < 8; i++) {
-        for (j = 0; j < 8; j++) {
-            printf("%c", a[i][j]);
-        }
-        printf("\n");
-    }
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
 }
