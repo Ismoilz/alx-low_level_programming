2bf78571cf02d1ff3d437e935ee17153d7b0b076
Ismoilz <ismailelhoumam1@gmail.com>
2023-07-05 18:28:33 -0700

    square root

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+* sq_calc - calculates sq
+* @sq: square
+* Returns: -1 if not natural
+*/
+
+int sq_calc(int sq)
+{
+
+	if (sq * sq == n)
+	{
+		return (sq);
+	}
+	if (sq * sq > n)
+	{
+		return (-1);
+	}
+	else
+		return (sqrt_value((sq + 1), n));
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number
+ * Returns: -1 if not natural
+ */
+int _sqrt_recursion(int n)
+{
+	return (sqrt_value(1, n));
+}
