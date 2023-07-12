7e6d3b94175b834391e880e62429443669a86613
Ismoilz <ismailelhoumam1@gmail.com>
2023-07-11 17:46:46 -0700

    fixing concat

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,27 +16,23 @@ char *str_concat(char *s1, char *s2)
 	s2 = "";
 
 	int len1;
+	int len2, i, j;
+	char *concatenated;
 
 	len1 = 0;
 
 	while (s1[len1] != '\0')
 	len1++;
 
-	int len2;
-
 	len2 = 0;
 
 	while (s2[len2] != '\0')
 	len2++;
 
-	char *concatenated;
-
 	concatenated = malloc((len1 + len2 + 1) * sizeof(char));
 
 	if (concatenated == NULL)
 	return (NULL);
-
-	int i, j;
 
 	for (i = 0; s1[i] != '\0'; i++)
 	concatenated[i] = s1[i];
