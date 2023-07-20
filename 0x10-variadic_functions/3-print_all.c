02b59ed55b3ab1bf49c54a0c73fd9c7d5ab1ce7e
Ismoilz <ismailelhoumam1@gmail.com>
2023-07-20 14:44:03 -0700

    fixing print all

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -11,43 +11,42 @@
 
 void print_all(const char * const format, ...)
 {
-	va_list args;
 	unsigned int i;
-	char *str;
-	const char *separator;
-
-	str = va_arg(args, char *);
-	separator = "";
-	i = 0;
+	va_list args;
+	char *s, *separator;
 
 	va_start(args, format);
 
+	separator = "";
+
+	i = 0;
 	while (format && format[i])
 	{
 		switch (format[i])
 		{
 			case 'c':
-			printf("%s%c", separator, va_arg(args, int));
-			break;
+				printf("%s%c", separator,  va_arg(args, int));
+				break;
 			case 'i':
-			printf("%s%d", separator, va_arg(args, int));
-			break;
+				printf("%s%d", separator, va_arg(args, int));
+				break;
 			case 'f':
-			printf("%s%f", separator, va_arg(args, double));
-			break;
+				printf("%s%f", separator, va_arg(args, double));
+				break;
 			case 's':
-			if (str == NULL)
-			str = "(nil)";
-			printf("%s%s", separator, str);
-			break;
+				s = va_arg(args, char *);
+				if (s == NULL)
+					s = "(nil)";
+				printf("%s%s", separator, s);
+				break;
 			default:
-			break;
+				i++;
+				continue;
 		}
 		separator = ", ";
 		i++;
 	}
 
-	va_end(args);
-
 	printf("\n");
+	va_end(args);
 }
