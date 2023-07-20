c49b3fa8874536a03f5c702da59eae5bd0dd04ce
Ismoilz <ismailelhoumam1@gmail.com>
2023-07-20 14:04:21 -0700

    adding print string

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,39 @@
+
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ *print_strings - prints string followed by a new line
+ *@separator: string between numbers
+ *@n: ints in the function
+ *Return: 0 if separator = NULL
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	const char *str;
+	const char *str;
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+	{
+		str = va_arg(args, const char *);
+
+		if (str != NULL)
+		printf("%s", str);
+		else
+	{
+		printf("(nil)");
+	}
+
+	if (separator != NULL && i < n - 1)
+		printf("%s", separator);
+	}
+
+	va_end(args);
+
+	printf("\n");
+}
