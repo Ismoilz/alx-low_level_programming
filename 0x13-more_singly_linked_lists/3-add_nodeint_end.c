6041f8d0f414e99967010f692445f9bb0ccaa2b2
Ismoilz <ismailelhoumam1@gmail.com>
2023-08-01 12:45:03 -0700

    Fix #1 for 4th task

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -25,9 +25,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	}
 	else
 	{
-		listint_t *temp = *head;
-		(temp->next != NULL)
+	listint_t *temp = *head;
+	while (temp->next != NULL)
+	{
 		temp = temp->next;
+	}
 
 		temp->next = new_node;
 	}
