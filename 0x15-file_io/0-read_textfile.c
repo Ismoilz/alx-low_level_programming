b659929c407aba9c441b637ea71a6f7f5a09c738
Ismoilz <ismailelhoumam1@gmail.com>
2023-08-07 14:02:29 -0700

    adding readme + 1st task

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Reads a text file and prints its content to standard output.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ * Return: The actual number of letters read and printed, or 0 on failure.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd = open(filename, O_RDONLY);
+	char *buffer = malloc(letters);
+	ssize_t bytes_read = read(fd, buffer, letters);
+	ssize_t bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
+
+	if (filename == NULL)
+	return (0);
+
+	if (fd == -1)
+	return (0);
+
+	if (buffer == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+
+	if (bytes_read == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	if (bytes_written == -1 || bytes_written != bytes_read)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+
+	free(buffer);
+	close(fd);
+	return (bytes_written);
+}
