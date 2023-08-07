c3cc4a9ac7c77fb37a9c840be5ce7232ff4fada4
Ismoilz <ismailelhoumam1@gmail.com>
2023-08-07 14:45:25 -0700

    Fix #1 3rd task

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -8,24 +8,30 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int fd = open(filename, O_WRONLY | O_APPEND);
-	ssize_t bytes_written = write(fd, text_content, strlen(text_content));
+	int fd, nletters, rwr;
 
-	if (filename == NULL)
+	if (!filename)
 	return (-1);
+
+	fd = open(filename, O_WRONLY | O_APPEND);
+
 	if (fd == -1)
 	return (-1);
 
-	if (text_content != NULL)
+	if (text_content)
 	{
+	for (nletters = 0; text_content[nletters]; nletters++);
 
-	if (bytes_written == -1)
+	rwr = write(fd, text_content, nletters);
+
+	if (rwr == -1)
 	{
-		close(fd);
-		return (-1);
+	close(fd);
+	return (-1);
 	}
 	}
 
 	close(fd);
+
 	return (1);
 }
