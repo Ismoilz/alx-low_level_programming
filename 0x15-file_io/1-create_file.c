b189b51cb4b362eb8ea326e54bb04857df24fa76
Ismoilz <ismailelhoumam1@gmail.com>
2023-09-04 18:51:31 -0700

    Fix#1 Task#2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
 #include "main.h"
 
 /**
@@ -13,7 +14,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd, write_result;
-	mode_t file_permissions = S_IRUSR | S_IWUSR; // rw-------
+	mode_t file_permissions = S_IRUSR | S_IWUSR;
 
 	if (filename == NULL)
 		return (-1);
